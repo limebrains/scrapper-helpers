f529c39b479b36c105fd7302fef7db6024ca545e
Wojciech Nowak <mail@pythonic.ninja>
2017-07-19 14:40:09 +0200

    added initial version of setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='scrapper_helpers',
+    version='0.0.1',
+    description='Scrapper helpers',
+    author='LimeBrains',
+    author_email='mail@limebrains.com',
+    url='https://github.com/limebrains/scrapper-helpers',
+    packages=['scrapper_helpers'],
+)
