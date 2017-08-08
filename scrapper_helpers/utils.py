217f5d991d12c80f4def09790bbb48dc4b1d8966
Arkadiusz Klein <arek.klein@limebrains.com>
2017-08-08 16:26:03 +0200

    Added get_random_user_agent and flatten

--- scrapper_helpers/utils.py
+++ scrapper_helpers/utils.py
@@ -3,6 +3,7 @@
 
 import os
 import pickle
+import random
 import hashlib
 import random
 import subprocess
@@ -19,6 +20,25 @@ MAX_FILENAME_LENGTH = os.environ.get(
     'MAX_FILENAME_LENGTH',
     int(subprocess.check_output("getconf NAME_MAX /", shell=True).strip())
 )
+
+USER_AGENTS = [
+    'Mozilla/5.0 (CrKey armv7l 1.5.16041) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+    '42.0.2311.135 Safari/537.36 Edge/12.246',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/'
+    '9.0.2 Safari/601.3.9',
+    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36'
+]
+
+
+def get_random_user_agent():
+    """ Randoms user agent to prevent "python" user agent
+
+    :return: Random user agent from USER_AGENTS
+    :rtype: str
+    """
+    return random.choice(USER_AGENTS)
 
 
 USER_AGENTS = [
