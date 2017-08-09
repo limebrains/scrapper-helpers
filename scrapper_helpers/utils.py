4b8ef8518a8c17cffcad8b9cc74b5cf461c2d0de
Wojtek Nowak <mail@pythonic.ninja>
2017-08-09 08:22:23 +0200

    Update utils.py

--- scrapper_helpers/utils.py
+++ scrapper_helpers/utils.py
@@ -116,7 +116,7 @@ def get_number_from_string(s, number_type, default):
     except ValueError:
         return default
 
-    
+
 def get_random_user_agent():
     """ Randoms user agent to prevent "python" user agent
     :return: Random user agent from USER_AGENTS
